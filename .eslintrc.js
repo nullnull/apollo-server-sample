0ebb9f62e30279d7cab1894530482ff6c063370b
Katsuma Narisawa <katsuma.narisawa@gmail.com>
2021-02-27 23:02:06 +0900

    fixup! Setup eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,36 +1,3 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: ['airbnb-base'],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 12,
-    sourceType: 'module',
-  },
-  plugins: ['@typescript-eslint'],
-  rules: {
-    'import/extensions': [
-      'error',
-      'ignorePackages',
-      {
-        js: 'never',
-        jsx: 'never',
-        ts: 'never',
-        tsx: 'never',
-      },
-    ],
-    'max-len': ['error', { code: 200 }],
-    'import/prefer-default-export': 0,
-    'no-unused-vars': 0,
-    'no-unreachable': 0,
-  },
-  settings: {
-    'import/resolver': {
-      node: {
-        extensions: ['.js', '.jsx', '.json', '.ts', '.tsx'],
-      },
-    },
-  },
+  extends: ['blitz'],
 }
